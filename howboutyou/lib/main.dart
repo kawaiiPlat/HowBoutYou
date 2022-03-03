16fa6534c181b3bd6c2ddd6c498f34ea0308537d
Jarrod Sanders <jarrod.sanders@ufl.edu>
2022-03-03 15:26:05 -0500

    fixing typo

--- howboutyou/lib/main.dart
+++ howboutyou/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:firebase_core/firebse_core.dart';
-import 'firebase_options.dart'
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
