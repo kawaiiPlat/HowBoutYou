1312ec2bb141b9131e244412b70be87d5ac3f4aa
YuyangHsieh8344 <yuyang.hsieh@ufl.edu>
2022-04-02 17:15:21 -0400

    input availbility part 1

--- howboutyou/lib/main.dart
+++ howboutyou/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+//import 'package:flutter_neat_and_clean_calendar/flutter_neat_and_clean_calendar.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Flutter Test Page'),
@@ -29,85 +23,138 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
+  //Function(String) callback;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  //Selects the given date programmatically in the SfCalendar by checking that the date falls in between the minimum and maximum date range
+  CalendarController _calendarController = CalendarController(); 
+
+  @override
+  initState() {
+    _calendarController.displayDate = DateTime(2021, 12, 08);
+    super.initState();
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SfCalendar(
+          view: CalendarView.week,
+          //callback
+           onViewChanged: (ViewChangedDetails details) {
+            List<DateTime> dates = details.visibleDates;
+          },
+           controller: _calendarController,
+           //calender date picker
+           showDatePickerButton: true,
+           //drag and drop
+            allowDragAndDrop: true,
+            onDragStart: dragStart,
+            onDragUpdate: dragUpdate,
+            dragAndDropSettings: DragAndDropSettings(showTimeIndicator: true),
+           //schedule view setting 
+           scheduleViewSettings: ScheduleViewSettings(
+            dayHeaderSettings: DayHeaderSettings(
+                dayFormat: 'EEEE',
+                width: 70,
+                dayTextStyle: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.red,
+                ),
+                dateTextStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.red,
+                ))),
+                //allow to change from day, week month
+           allowedViews: <CalendarView>[
+              CalendarView.day,
+              CalendarView.week,
+              CalendarView.month,
+              CalendarView.schedule
+            ],
+            
+            monthViewSettings: MonthViewSettings(
+              appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+            //the minimal day that we can go back to
+           minDate: DateTime(2021, 12, 31, 0 , 0, 0),
+
+           //appointment text style
+            appointmentTextStyle: TextStyle(
+              fontSize: 25,
+              color: Color.fromARGB(255, 6, 9, 185),
+              letterSpacing: 1,
+              fontWeight: FontWeight.bold),
+
+          //data source
+          dataSource: _getDataSource(), 
+          
+         
+        )
+        );
+    }        
+}
+
+
+_AppointmentDataSource _getDataSource() {
+  List<Appointment> appointments = <Appointment>[];
+  appointments.add(Appointment(
+    startTime: DateTime.now(),
+    endTime: DateTime.now().add(Duration(minutes: 10)),
+    subject: 'Meeting',
+    color: Colors.blue,
+    startTimeZone: '',
+    endTimeZone: '',
+  ));
+
+  return _AppointmentDataSource(appointments);
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source){
+   appointments = source; 
   }
+}
+
+class Meeting {
+  Meeting(
+      {this.eventName = '',
+      required this.from,
+      required this.to,
+      required this.background,
+      this.isAllDay = false,
+      this.recurrenceRule,
+      this.exceptionDates});
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
+  String? recurrenceRule;
+  List<DateTime>? exceptionDates;
+}
+
+
+
+void dragStart(AppointmentDragStartDetails appointmentDragStartDetails) {
+  dynamic appointment = appointmentDragStartDetails.appointment;
+  CalendarResource? resource = appointmentDragStartDetails.resource;
+}
+
+void dragUpdate(AppointmentDragUpdateDetails appointmentDragUpdateDetails) {
+  dynamic appointment = appointmentDragUpdateDetails.appointment;
+  DateTime? draggingTime = appointmentDragUpdateDetails.draggingTime;
+  Offset? draggingOffset = appointmentDragUpdateDetails.draggingPosition;
+  CalendarResource? sourceResource = appointmentDragUpdateDetails.sourceResource;
+  CalendarResource? targetResource = appointmentDragUpdateDetails.targetResource;
 }
